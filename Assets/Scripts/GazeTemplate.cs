12185ff31f8310546da0933ed4045d93c6339b94
Joe Wise <joedavidwise@gmail.com>
2017-07-11 17:13:05 -0700

    SneezingTree prefab completed. Gaze at the tree, particles spiral around it and animation plays.

---
+++ Assets/Scripts/GazeTemplate.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+public class GazeTemplate : MonoBehaviour {
+
+    [SerializeField] private VRInteractiveItem m_InteractiveItem;
+
+    public float _duration = 1f;
+    public bool _isCounting = false;
+    public float _count;
+
+    private void Update()
+    {
+        if (_isCounting)
+        {
+            _count += Time.deltaTime;
+            if (_count > _duration)
+            {
+                //do somethingafter gazing for the duration
+            }
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        m_InteractiveItem.OnOver += HandleOver;
+        m_InteractiveItem.OnOut += HandleOut;
+        m_InteractiveItem.OnClick += HandleClick;
+        m_InteractiveItem.OnDoubleClick += HandleDoubleClick;
+    }
+
+
+    private void OnDisable()
+    {
+        m_InteractiveItem.OnOver -= HandleOver;
+        m_InteractiveItem.OnOut -= HandleOut;
+        m_InteractiveItem.OnClick -= HandleClick;
+        m_InteractiveItem.OnDoubleClick -= HandleDoubleClick;
+    }
+
+
+    //Handle the Over event
+    private void HandleOver()
+    {
+        //Debug.Log("Show over state");
+        if (!_isCounting)
+        {
+            _isCounting = true;
+            
+        }
+    }
+
+
+    //Handle the Out event
+    private void HandleOut()
+    {
+        //Debug.Log("Show out state");
+        _isCounting = false;
+        _count = 0.0f;
+    }
+
+
+    //Handle the Click event
+    private void HandleClick()
+    {
+        //Debug.Log("Show click state");
+
+    }
+
+
+    //Handle the DoubleClick event
+    private void HandleDoubleClick()
+    {
+        //Debug.Log("Show double click");
+
+    }
+}
---
+++ Assets/Scripts/SpiralParticle.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiralParticle : MonoBehaviour {
+
+    public int frequency = 1;
+    public float resolution = 20.0f;
+    public float amplitude = 1.0f;
+    public float zValue = 0.0f;
+
+    ParticleSystem ps;
+
+    // Use this for initialization
+    void Start () {
+        ps = GetComponent<ParticleSystem>();
+        CreateSpiral();
+	}
+	
+    public void CreateSpiral()
+    {
+        //ParticleSystem ps = GetComponent<ParticleSystem>();
+        var vel = ps.velocityOverLifetime;
+        vel.enabled = true;
+        vel.space = ParticleSystemSimulationSpace.Local;
+
+        ps.startSpeed = 0.0f;
+
+        vel.z = new ParticleSystem.MinMaxCurve(10.0f, zValue);
+
+        AnimationCurve curveX = new AnimationCurve();
+        for (int i = 0; i < resolution; i++)
+        {
+            float newTime = (i / (resolution - 1));
+            float myValue = amplitude * Mathf.Sin(newTime * (frequency * 2) * Mathf.PI);
+
+            curveX.AddKey(newTime, myValue);
+
+        }
+
+        vel.x = new ParticleSystem.MinMaxCurve(10.0f, curveX);
+
+
+        AnimationCurve curveY = new AnimationCurve();
+        for (int i = 0; i < resolution; i++)
+        {
+            float newTime = (i / (resolution - 1));
+            float myValue = amplitude * Mathf.Cos(newTime * (frequency * 2) * Mathf.PI);
+
+            curveY.AddKey(newTime, myValue);
+
+        }
+
+        vel.y = new ParticleSystem.MinMaxCurve(10.0f, curveY);
+
+        //ps.Play();
+    }
+
+    public void Play()
+    {
+        //Debug.Log("play spiral particle");
+        ps.Play();
+    }
+
+    public void Stop()
+    {
+        //Debug.Log("stop spiral particle");
+        ps.Stop();
+    }
+
+    public void Emit(int amount)
+    {
+        //Debug.Log("emit spiral particle");
+        ps.Emit(amount);
+    }
+
+    public bool IsPlaying()
+    {
+        return ps.isPlaying;
+    }
+
+    public int GetParticleCount()
+    {
+        return ps.particleCount;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
