bed0a44bf1ef26d762bf452d58d4db4770aa66a5
Deena <dbaum@usc.edu>
2017-08-03 17:05:38 -0700

    debug Keys

---
+++ Assets/DebugKeys.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugKeys : MonoBehaviour {
+
+    [SerializeField] AudioSource _voSource;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //reset
+            SceneManager.LoadScene("GazeboStart");
+        } else if (Input.GetKeyDown(KeyCode.M))
+        {
+            // mute VO
+            if(_voSource.volume == 0.0f)
+            {
+                _voSource.volume = 1.0f;
+            } else
+            {
+                _voSource.volume = 0.0f;
+            }
+           
+        } else if (Input.GetKeyDown(KeyCode.E))
+        {
+            //reload this scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+}
