e3d64aac0e8b8f825618a91fc7ab921fca5a1881
Sikandar Sidhu <sikandas@usc.edu>
2017-08-08 16:53:36 -0700

    Scene Transition Fixes

--- Assets/DebugKeys.cs
+++ Assets/DebugKeys.cs
@@ -9,29 +9,25 @@ public class DebugKeys : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            //reset
-            SceneManager.LoadScene("GazeboStart");
-        } else if (Input.GetKeyDown(KeyCode.M))
-        {
-            // mute VO
-            if(_voSource.volume == 0.0f)
-            {
-                _voSource.volume = 1.0f;
-            } else
-            {
-                _voSource.volume = 0.0f;
-            }
+		if (Input.GetKeyDown (KeyCode.R)) {
+			//reset
+			SceneManager.LoadScene ("GazeboStart");
+		} else if (Input.GetKeyDown (KeyCode.M)) {
+			// mute VO
+			if (_voSource.volume == 0.0f) {
+				_voSource.volume = 1.0f;
+			} else {
+				_voSource.volume = 0.0f;
+			}
            
-        } else if (Input.GetKeyDown(KeyCode.E))
-        {
-            //reload this scene
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        } else if (Input.GetKeyDown(KeyCode.D))
-        {
-            //load Deena forest
-            SceneManager.LoadScene("Forest_Deena");
-        }
+		} else if (Input.GetKeyDown (KeyCode.E)) {
+			//reload this scene
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		} else if (Input.GetKeyDown (KeyCode.D)) {
+			//load Deena forest
+			SceneManager.LoadScene ("Forest_Deena");
+		} else if (Input.GetKeyDown (KeyCode.W)) {
+			SceneManager.LoadScene ("Forest");
+		}
 	}
 }
